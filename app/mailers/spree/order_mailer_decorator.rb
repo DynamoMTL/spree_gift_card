0b9136e129687df10d6bc3f05957c0e12ed78ac1
Szabolcs Pap <spap26@gmail.com>
2013-10-07 10:52:47 -0400

    Personalise giftcard's email

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -2,7 +2,7 @@ Spree::OrderMailer.class_eval do
   def gift_card_email(card, order)
     @gift_card = card
     @order = order
-    subject = "#{Spree::Config[:site_name]} Gift Card"
+    subject = "#{order.name} got you a Blue Bottle Coffee gift card!"
     @gift_card.update_attribute(:sent_at, Time.now)
     mail(to: card.email, from: from_address, subject: subject)
   end
