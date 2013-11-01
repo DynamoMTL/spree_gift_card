5115128a68db3bf0ceedc85e7bd9b1405d1a700e
Daniel Wright <daniel@godynamo.com>
2013-11-01 17:13:28 -0400

    Compute gift-card adjustment using only eligible adjustments

--- app/models/spree/calculator/gift_card.rb
+++ app/models/spree/calculator/gift_card.rb
@@ -11,7 +11,7 @@ module Spree
       def compute(order, gift_card)
         # Ensure a negative amount which does not exceed the sum of the order's item_total, ship_total, and 
         # tax_total, minus other credits.
-        credits = order.adjustments.select{|a|a.amount < 0 && a.originator_type != 'Spree::GiftCard'}.map(&:amount).sum
+        credits = order.adjustments.eligible.select{|a|a.amount < 0 && a.originator_type != 'Spree::GiftCard'}.map(&:amount).sum
         [(order.item_total + order.ship_total + order.tax_total + credits), gift_card.current_value].min * -1
       end
 
